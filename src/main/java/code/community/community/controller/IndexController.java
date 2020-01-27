5de08792cc1e7adb47971d8f5ce1242ee8b6d33a
Achillesxx <343487870@qq.com>
2020-01-27 13:24:40 +0800

    add profile

--- src/main/java/code/community/community/controller/IndexController.java
+++ src/main/java/code/community/community/controller/IndexController.java
@@ -1,22 +1,34 @@
 package code.community.community.controller;
 
 import code.community.community.Mapper.UserMapper;
+import code.community.community.dto.PageDTO;
+import code.community.community.dto.QuestionDTO;
 import code.community.community.model.User;
+import code.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired(required = false)
+    private QuestionService questionService;
 
     @Autowired(required=false)
     private UserMapper userMapper;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name="page",defaultValue = "1")Integer page,
+                        @RequestParam(name="size",defaultValue = "5")Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
         for(Cookie cookie : cookies){
@@ -29,7 +41,8 @@ public class IndexController {
                 break;
             }
         }
-
+        PageDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
---
+++ src/main/java/code/community/community/service/QuestionService.java
@@ -0,0 +1,95 @@
+package code.community.community.service;
+
+import code.community.community.Mapper.QuestionMapper;
+import code.community.community.Mapper.UserMapper;
+import code.community.community.dto.PageDTO;
+import code.community.community.dto.QuestionDTO;
+import code.community.community.model.Question;
+import code.community.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
+    @Autowired(required = false)
+    private QuestionMapper questionMapper;
+
+    public PageDTO list(Integer page, Integer size) {
+
+        Integer totalPage;
+        PageDTO pageDTO = new PageDTO();
+        Integer totalCount = questionMapper.count();
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+        pageDTO.setPageination(totalPage, page);
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        pageDTO.setQuestions(questionDTOList);
+        return pageDTO;
+    }
+
+    public PageDTO list(Integer userId, Integer page, Integer size) {
+        PageDTO pageDTO = new PageDTO();
+
+        Integer totalPage;
+
+        Integer totalCount = questionMapper.countByUserId(userId);
+
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+        pageDTO.setPageination(totalPage, page);
+
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userId, offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+
+        pageDTO.setQuestions(questionDTOList);
+        return pageDTO;
+    }
+}
