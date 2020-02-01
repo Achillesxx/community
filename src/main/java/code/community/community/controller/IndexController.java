95af5d7188a8095719955439c6d2362baac0858e
Achillesxx <343487870@qq.com>
2020-02-01 19:31:09 +0800

    add profile

--- src/main/java/code/community/community/controller/IndexController.java
+++ src/main/java/code/community/community/controller/IndexController.java
@@ -1,9 +1,6 @@
 package code.community.community.controller;
 
-import code.community.community.Mapper.UserMapper;
 import code.community.community.dto.PageDTO;
-import code.community.community.dto.QuestionDTO;
-import code.community.community.model.User;
 import code.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,36 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 @Controller
 public class IndexController {
 
     @Autowired(required = false)
     private QuestionService questionService;
 
-    @Autowired(required=false)
-    private UserMapper userMapper;
-
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name="page",defaultValue = "1")Integer page,
                         @RequestParam(name="size",defaultValue = "5")Integer size){
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user =userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
         PageDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
--- src/main/java/code/community/community/controller/PublishController.java
+++ src/main/java/code/community/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package code.community.community.controller;
 
 import code.community.community.Mapper.QuestionMapper;
-import code.community.community.Mapper.UserMapper;
 import code.community.community.model.Question;
 import code.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired(required=false)
     private QuestionMapper questionMapper;
-
-    @Autowired(required=false)
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -48,19 +43,7 @@ public class PublishController {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for(Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                user =userMapper.findByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
 
         if(user == null){
             model.addAttribute("error","用户未登录");
